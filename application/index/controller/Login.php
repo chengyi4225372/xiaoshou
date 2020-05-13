d48107bb4e458db050333c234751e02e6cc9c822
chengyi <732345907@qq.com>
2020-05-13 19:25:01 +0800

    add

---
+++ application/index/controller/Login.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2020/5/13 0013
+ * Time: 19:20
+ */
+namespace app\index\controller;
+
+use think\Controller;
+use think\Db;
+use think\Request;
+
+class Login extends Controller{
+
+    public function login(){
+        if($this->request->isGet()){
+            return $this->fetch();
+        }
+
+        if($this->request->isPost()){
+
+        }
+
+        return false;
+    }
+}
